3a333afc6acb3290deaf7cc4c7b6cab296dac7c7
tensorab <s1810576118@ru.ac.bd>
2021-04-27 17:03:53 +0600

    Learning about useReducer Hook

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const COUNTER_INCREAMENT = 1
+
+const reducer = (state, action) => {
+    return {...state, counter: state.counter + action.payload}
+}
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { counter: 0 })
 
     return (
         <View>
             <Button title="Increase" onPress={() => {
-                setCounter(counter+1);
+                dispatch({type: "Increase", payload: COUNTER_INCREAMENT})
             }}/>
             <Button title="Decrease" onPress={() => {
-                setCounter(counter-1);
+                dispatch({type: "Decrease", payload: -1 * COUNTER_INCREAMENT})
             }}/>
-            <Text>Current Count:{counter}</Text>
+            <Text>Current Count:{state.counter}</Text>
 
         </View>
     );
